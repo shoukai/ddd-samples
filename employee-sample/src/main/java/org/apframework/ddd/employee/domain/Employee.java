0003cc75245ed0801a91a5343bfcfd2d9ca75493
Shoukai Huang <huangshoukai@renrenche.com>
2020-05-26 01:11:09 +0800

    add skill list

--- employee-sample/src/main/java/org/apframework/ddd/employee/domain/Employee.java
+++ employee-sample/src/main/java/org/apframework/ddd/employee/domain/Employee.java
@@ -6,10 +6,13 @@ import org.apframework.ddd.employee.domain.entity.EmployeeEntity;
 import org.apframework.ddd.employee.domain.factories.EmployeeFactory;
 import org.apframework.ddd.employee.domain.dto.EmployeeSkillResDTO;
 import org.apframework.ddd.employee.domain.repository.mapper.EmployeeMapper;
+import org.apframework.ddd.employee.domain.vo.EmployeeSkill;
 import org.apframework.ddd.employee.infrastructure.exception.BizException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 @Builder
@@ -36,19 +39,19 @@ public class Employee {
     }
 
     @Transactional
-    public Long entry(EmployeeEntryDTO employeeEntry) {
+    public Long entry(EmployeeEntryDTO employeeEntryDTO) {
         // 创建者
-        EmployeeEntity employeeEntity = EmployeeFactory.with(employeeEntry);
+        EmployeeEntity employeeEntity = EmployeeFactory.with(employeeEntryDTO);
         int result = employeeMapper.insert(employeeEntity);
         if (result < 1) {
             throw new BizException("存储失败");
         }
-        Long id = employeeEntity.getId();
-
+        List<EmployeeSkill> employeeSkillList= EmployeeFactory.withSkill(employeeEntity.getId(), employeeEntryDTO);
+        employeeMapper.saveEmployeeSkillList(employeeSkillList);
 
         // MAPPER SAVE
         // RETURN ID;
         // notify employee add done
-        return id;
+        return employeeEntity.getId();
     }
 }
--- employee-sample/src/main/java/org/apframework/ddd/employee/domain/repository/mapper/EmployeeMapper.java
+++ employee-sample/src/main/java/org/apframework/ddd/employee/domain/repository/mapper/EmployeeMapper.java
@@ -1,6 +1,7 @@
 package org.apframework.ddd.employee.domain.repository.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Param;
 import org.apframework.ddd.employee.domain.entity.EmployeeEntity;
 import org.apframework.ddd.employee.domain.vo.EmployeeSkill;
 
@@ -8,6 +9,6 @@ import java.util.List;
 
 public interface EmployeeMapper extends BaseMapper<EmployeeEntity> {
 
-    int saveEmployeeSkillList(List<EmployeeSkill> skillList);
+    int saveEmployeeSkillList(@Param("skillList") List<EmployeeSkill> skillList);
 
 }
